63bc959223587d5e9faaf9bb7d36eb9c863eafa6
ryad-ht <ryad31ht@gmail.com>
2023-12-08 16:02:04 +0100

    a

---
+++ stats.r
@@ -0,0 +1,27 @@
+data <-read.csv("/Users/ryadhadj-tahar/housepredict/housing_price_dataset.csv")
+summary(data)
+
+
+head(data)
+summary(data)
+
+# Vérification des valeurs manquantes
+sapply(data, function(x) sum(is.na(x)))
+
+# Analyse de corrélation
+correlation_matrix <- cor(data)
+print(correlation_matrix)
+
+# Construction du modèle de régression linéaire multiple
+modele_regression <- lm(Price ~ SquareFeet + Bedrooms + Bathrooms + Neighborhood + YearBuilt, data = data)
+
+# Résumé du modèle
+summary(modele_regression)
+
+# Évaluation du modèle
+predicted_prices <- predict(modele_regression, data)
+RMSE <- sqrt(mean((data$Price - predicted_prices)^2))
+R_squared <- summary(modele_regression)$r.squared
+
+print(paste("RMSE :", RMSE))
+print(paste("R-squared :", R_squared))
